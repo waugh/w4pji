fdeaf4dfdeecdd4e4483b018ef4c7366d66b93e9
Jack Waugh <m2hh2kmhsn@snkmail.com>
2015-12-07 10:41:55 -0500

    Questions and proposed edits on 'real effect of sanctions' article.

--- src/our_articles/2015-12-06_effect_of_sanctions.html.rb
+++ src/our_articles/2015-12-06_effect_of_sanctions.html.rb
@@ -8,9 +8,9 @@ I have mentioned in other talks and writings, the
 Deal only covers the nuclear related sanctions on Iran.
 Iran has been under increasing sanctions and boycotts since
 its revolution.  The first sanctions were military at the
-beginning of the revolution, while Iraq with total military
-support by the west attacked Iran. Since Iran's military
-arsenal from the time of the shah were American, Iran could
+beginning of the revolution, while Iraq, with total military
+support by the West, attacked Iran. Since Iran's military
+arsenal from the time of the Shah were American, Iran could
 not get even parts, let alone, new arsenal.  However, with
 the revolutionary spirit, the Iranians reverse engineered
 or invented new ways to provide the military arsenal for
@@ -19,105 +19,126 @@ US behind it including chemical weapons used against Iran's
 army. So, Iran was capable of defeating this aggression
 just one year after revolution and under military boycott.
 One reason, in addition to the revolutionary determination
-and creativity, was that Iran is not  a poor country due to
+and creativity, was that Iran is not  a poor country, due to
 its abundant oil.  In fact the Iran-Contra affair belongs
-to that period where the colnol North and anti Nicaragua
-forces in US, conspired to sell arms to Iran(in violation
-of US embargo) so that they could use the revenue from
-the sale in their contra activities in Nicaragua.( so much
+to that period where the Colonel North and anti-Nicaragua
+forces in the US conspired to sell arms to Iran (in violation
+of a US embargo) so that they could use the revenue from
+the sale in their contra activities in Nicaragua (so much
 for lack of conspiracy in US international intrigue).
 !
 end
 
 p <<-'!'
-US continuously increased sanctions against Iran based on
-various false claims. Iran was declared a State sponsor
+The US continuously increased sanctions against Iran based on
+various false claims. Iran was declared a state sponsor
 of terrorism because of its defense of the Palestinian
-and Lebanese resistance forces. This state- ponsorship of
-terrorism of Iran has not been lifted in the Deal either.
+and Lebanese resistance forces.
+This designation of Iran as a state sponsor of terrorism
+has not been lifted in the Deal either.
 !
 
 p <<-'!'
 Then in 2002, when Iran in the reconstruction after
 the 8-year war decided to restart its nuclear industry,
-it gave the US the best excuse and for 12 years it kept
+it gave the US the best excuse, and for 12 years, the US kept
 piling on the sanctions. Even during the government of
-Khatami( 1997-2005), when he unilaterally stopped uranium
-enrichment, US not only refused to lift any sanctions but
-kept increasing them.  While US claimed it was concerned
+Khatami (1997-2005), when he unilaterally stopped uranium
+enrichment, the US not only refused to lift any sanctions but
+kept increasing them.  While the US claimed it was concerned
 about Iran's enrichment, it really was using the false
-issue to bring Iran under its sphere of control. So,
+issue to bring Iran under its sphere of control.
+
+[Can't guess what you mean here:]
+
+So,
 just stopping the enrichment was not enough recuse Iran
 said it would not negotiate anything else with any other
-ore-conditions.  The US wanted other advantages like the
-one we see in the new deal. for example, at the time,
+preconditions.
+
+[]
+
+The US wanted other advantages like the
+one we see in the new deal. For example, at the time,
 Iran was not accepting inspection of its military sites,
-and so on.  When Ahmadi nejad was elected in 2005, the
-government took a hard anti-imperialist position towards
-US. Iran said since for 2 years of enrichment  stoppage,
-US had increased its belligerence, Iran was going to reopen
-its enrichment program and expand it so it would become
-nuclear industry independent.  In addition Iran went on
-to develop strong relationship with all Latin American
-anti imperialist forces such as cuba, Venezuela, Bolivia
-and.. and strengthened its relationship with Lebanon,
-Syria and Palestine.  In response to US efforts to isolate
-Iran economically, Ahmadi Nejad's government developed
-its ties with all non European countries and Russia. iran
-was not economically isolated.  In fact, to my chagrin,
-you could fine anything in Iran, including rel Gucci backs
-and glasses( My daughter's sun glass broke while in Iran
-and we went to a boutique near my brother's house and
-were amazed at all the western designee glasses that we
-could not afford).  The people of lower economic classes
-do not buy western consumer goods anyway.  because of the
-international market of global capitalism, big corporations
-have international ways of selling things anywhere. You
-could even buy Pepsi cola in Iran.  lower economic classes
-are affected by prices of basic goods. The biggest blow
-was after the victory of Ahmadi Nejad in 2009. Then US was
-determined to increase the pressure and turn people against
-the government. So, the US9with the help of UN ) imposed
-banking restrictions on Iran. This meant that because of
-lack of Internationale banking access, Iran was not able to
-obtain letters of credit which is a must in international
-trade, and the value of its currency dropped right away
-by 200%. This made imports more expensive, but since then
-I traveled to Iran many times and there was never any
-shortage of basic food or luxury.  In response to this
-the government increased subsidies to the poor and bread
-subsidies( these were reduced or eliminated by the current
-government). The government also increased the minimum
-wage and the government employees pay and the pensioners.
-The criticism of the opposition was that this increases
-the inflation(just as neoliberals everywhere say about
-aid to the poor). But the terrible inflation was to a
-large extent due to international banking sanctions.
-So, there was some effect but there were no shortages
-and since a large % of Iranians work in the government,
-the increase in government pay is very helpful.
+and so on.
 !
 
 p <<-'!'
-Benefits of the sanctions: since Iran has oil revenue and
+When Ahmadinejad was elected in 2005, the
+government took a hard anti-imperialist position towards the
+US. Iran said that since for 2 years of enrichment  stoppage, the
+US had increased its belligerence, Iran was going to reopen
+its enrichment program and expand it so it would become
+nuclear industry independent.  In addition Iran went on
+to develop strong relationships with all Latin American
+anti-imperialist forces such as Cuba, Venezuela, and Bolivia,
+and strengthened its relationships with Lebanon,
+Syria and Palestine.  In response to US efforts to isolate
+Iran economically, Ahmadinejad's government developed
+its ties with all non-European countries and with Russia. Iran
+was not economically isolated.  In fact, to my chagrin,
+you could find anything in Iran, including real Gucci bags
+and glasses (my daughter's sunglasses broke while we were in Iran
+and we went to a boutique near my brother's house and
+were amazed at all the western-designed glasses that we
+could not afford).  The people of lower economic classes
+do not buy western consumer goods anyway.  Because of the
+international market of global capitalism, big corporations
+have international ways of selling things anywhere. You
+could even buy Pepsi cola in Iran.  Lower economic classes
+are affected by prices of basic goods. The biggest blow
+was after the victory of Ahmadinejad in 2009. Then the US was
+determined to increase the pressure and turn people against
+the government. So, the US (with the help of the UN) imposed
+banking restrictions on Iran. This meant that because of
+lack of international banking access, Iran was not able to
+obtain letters of credit, which is a must in international
+trade, and the value of its currency dropped right away
+by 200%.
+
+[What does this mean? If the number of USD to buy one Rial was x prior to the
+drop, what is the formula for how much it was after? It could not have gone
+negative!]
+
+This made imports more expensive, but since then
+I traveled to Iran many times and there was never any
+shortage of basic food or luxury.  In response to this
+the government increased subsidies to the poor and bread
+subsidies (these have since been reduced or eliminated by the current
+government). The government also increased the minimum
+wage, the government employees' pay, and pensions.
+The criticism from the opposition was that this increases
+inflation (just as neoliberals everywhere say about
+aid to the poor). But the terrible inflation was to a
+large extent due to international banking sanctions.
+So, there was some effect but there were no shortages
+and since a large proportion of Iranians work in the government,
+the increase in government pay is very helpful.
+!
+
+h2 "Benefits of the Sanctions" # This makes this phrase a header.
+
+p <<-'!'
+Since Iran has oil revenue and a
 highly educated populace, under 30 years of sanctions it
 has become more self sufficient in many areas.
 !
 
 p <<-'!'
 The most important benefit has occurred in the
-pharmaceutical industry. Every medicine(other than the
-most experimental ones being developed in the west)
-is produced in Iran generically and cheaply. the drug
-prices are highly controlled and subsidized. As a result
+pharmaceutical industry. Every medicine (other than the
+most experimental ones being developed in the West)
+is produced in Iran generically and cheaply. The drug
+prices are highly controlled and subsidized. As a result,
 drugs are very cheap in Iran. The first time I went to buy
 something I was shocked because a tube of the medicine was
 less than 50 cents. So, many Iranians when traveling to
 Iran buy up to three months of all their medications and
-bring it here. We did the same for my Mother and I refilled
+bring it here. We did the same for my mother, and I refilled
 some of my prescriptions there. I am afraid that with the
-opening of the doors to western companies this important
-achievement will be at risk.  Along die the pharmaceutical
+opening of the doors to western companies, this important
+achievement will be at risk.  Along with the pharmaceutical
 industry, health services have also developed and people
 from various middle eastern countries, especially Iraq
 and Afghanistan where their health system is in tatters,
@@ -126,7 +147,7 @@ come to Iran for surgeries.
 
 p <<-'!'
 Even though Iranians with money drive Japanese, Korean
-and German cars, but due to sanctions Iranian automobile
+and German cars, due to the sanctions the Iranian automobile
 industry expanded and Iran exports cars in the middle
 east. Again the opening of doors will endanger this
 industry as well.
@@ -134,77 +155,79 @@ industry as well.
 
 p <<-'!'
 This kind of development has been true for many small
-manufacturing business, those which build metal oil
+manufacturing businesses, those that build metal oil
 containers, and such.
 !
 
 p <<-'!'
-Iran's ceramic industry especially factories located in
-Yazd( When I visited Yazd about 4 years ago there was
+Iran's ceramic industry, especially factories located in
+Yazd, have expanded and Iran is a big exporter
+of ceramics both for housing construction and for home goods.
+(When I visited Yazd about 4 years ago there was
 almost full employment there. Since it is a small city
-near the desert, the land is cheap to build factories and
+near the desert, the land is cheap to build factories on and
 as a result housing is cheap, so it is a good place for
-factory workers.)have expanded and Iran is big exporter
-of ceramics both for housing construction and home goods.
+factory workers.)
 !
 
 p <<-'!'
-Since Iran's market. like everywhere else has been open
-to china, all manners of consumer goods at low prices,
-made in china, are available in Iran. So, textile induxtry
-like most countries in Africa has not been revived.
+Since Iran's market, like everywhere else, has been open
+to China, all manners of consumer goods at low prices,
+made in China, are available in Iran. So, the textile induxtry
+has not been revived in Iran (just as it also has not been revived
+in most countries in Africa).
 !
 
 p <<-'!'
-Since the poor when buying foreign made products, buy
+Since the poor when buying foreign-made products, buy
 Chinese, they were not affected as much. After the banking
 sanctions, Iran and China did contracts in barter. Iranians
 complained because they said we gave then gas and they gave
-us junk goods. Then I asked them why they bough Chinese,
+us junk goods. Then I asked them why they bought Chinese,
 instead of Iranian, and they said because Chinese was
-cheaper. Same story here in US.
+cheaper. Same story here in the US.
 !
 
 p <<-'!'
 Since Iran produces lots of fruits and vegetables, it
-exports those to the gulf countries where they are deserts
+exports those to the gulf countries that are essentially deserts
 and import all produce.
 !
 
 p <<-'!'
 Another benefit from my point of view was that the banking
 sanctions made it somewhat more difficult for the 1%
-to take all the money they teal from Iran, to foreign
+to take all the money they steal from Iran, to foreign
 countries. It simply became more expensive in terms if
-exchange and the extra bribes they had to pay in the black
-international king market to transfect all their stolen
+exchange and the extra bribes they had to pay in the
+international black market to transfect all their stolen
 wealth to outside Iran.
 !
 
 p <<-'!'
 Even in areas of government contracts for building of
 railroads and subways and such big public works, Iranian
-small contractors have been doing it( I have very close
+small contractors have been doing it (I have very close
 family involved both in small industrial business and
-public work contracts for subways and such), with the
+public-works contracts for subways and such), with the
 opening of borders to foreign companies which I suspect,
 under the pretense of helping Iran, will demand credits
-from Iran's government, these iranian small industrial
-shops and engineering contractors  will lose out and
-foreign engineers and managers will take lucrative pays
+from Iran's government, these Iranian small industrial
+shops and engineering contractors  shall lose out and
+foreign engineers and managers will take lucrative pay
 and displace the Iranians. This is what always happens when
-countries of the global south open their borders to foreign
-"investment". Remember Arbantina and Greece.
+countries of the the global south open their borders to foreign
+"investment". Remember Argentina and Greece.
 !
 
 p <<-'!'
-So, Iran is not an isolated, desperate and Hungary. It
-did not have to give up its nuclear energy rights and
-its military independence to get a promise of US to lift
-some sanctions after Iran has destroyed million dollars
-of nuclear industry capital investments( bulldozing the
+So, Iran is not isolated, desperate and hungry. It
+did not have to give up its nuclear-energy rights and
+its military independence to get a promise from the US to lift
+some sanctions after Iran has destroyed millions of dollars
+of nuclear-industry capital investments (bulldozing the
 constructions) and has bent over backward to satisfy the
-most sever demands by US. And still be on probation under
+most severe demands by the US, and still be on probation under
 the threat of military attack.
 !
 end
